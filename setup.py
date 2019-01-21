c6037c41932fc5f77ca46a543741e01c9cfff8ef
defnngj <353332934@qq.com>
2019-01-21 16:27:47 +0800

    更新目录版本读取

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('page_objects/__init__.py', 'rb') as f:
+with open('poium/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
