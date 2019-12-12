116ae413baff4e4e429f7312d98ffaaac99c3209
fnngj <fnngj@126.com>
2019-12-13 00:29:51 +0800

    uiautomator2 API

--- poium/u2/__init__.py
+++ poium/u2/__init__.py
@@ -85,6 +85,19 @@ class PageElement(object):
             raise NameError("No corresponding element was found")
         driver(**self.kwargs).set_text(text=text)
 
+    def send_keys(self, text):
+        """
+        alias of set_text
+        :param text:
+        """
+        return self.set_text(text)
+
+    def clear_text(self):
+        """
+        Clear the text
+        """
+        return self.set_text(None)
+
     def get_text(self):
         """
         get element text
@@ -98,3 +111,57 @@ class PageElement(object):
         else:
             raise NameError("No corresponding element was found")
         return driver(**self.kwargs).get_text()
+
+    def bounds(self):
+        """
+        Returns the element coordinate position
+        :return: left_top_x, left_top_y, right_bottom_x, right_bottom_y
+        """
+        global driver
+        for i in range(self.time_out):
+            if driver(**self.kwargs).exists:
+                break
+            else:
+                time.sleep(1)
+        else:
+            raise NameError("No corresponding element was found")
+        return driver(**self.kwargs).bounds()
+
+    def center(self):
+        """
+        Returns the center coordinates of the element
+        return: center point (x, y)
+        """
+        global driver
+        for i in range(self.time_out):
+            if driver(**self.kwargs).exists:
+                break
+            else:
+                time.sleep(1)
+        else:
+            raise NameError("No corresponding element was found")
+        return driver(**self.kwargs).center()
+
+    def swipe(self, direction, steps=1):
+        """
+        Performs the swipe action on the UiObject.
+        Swipe from center
+        :param direction: "left", "right", "up", "down"
+        :param steps: move steps, one step is about 5ms
+        """
+        global driver
+        for i in range(self.time_out):
+            if driver(**self.kwargs).exists:
+                break
+            else:
+                time.sleep(1)
+        else:
+            raise NameError("No corresponding element was found")
+        return driver(**self.kwargs).swipe(direction, steps)
+
+    def wait(self):
+        """
+        Wait until UI Element exists or gone
+        """
+        global driver
+        return driver(**self.kwargs).wait(exists=True, timeout=self.time_out)
