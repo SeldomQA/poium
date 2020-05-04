f5c77d8cf8303ce989877320b5ea458aeb7958ec
fnngj <fnngj@126.com>
2020-05-04 17:38:52 +0800

    update selenium sample

--- sample/selenium_sample/test_select.py
+++ sample/selenium_sample/test_select.py
@@ -1,11 +1,11 @@
-from poium import Page, PageSelect
+from poium import Page
 from poium import NewPageElement as PageElement
 from time import sleep
 
 
 class SelectPage(Page):
     frame = PageElement(id_="iframeResult", describe="表单")
-    elm = PageElement(xpath="//select", describe="选择框")
+    select_elem = PageElement(xpath="//select", describe="选择框")
 
 
 def test_select(browser):
@@ -18,10 +18,10 @@ def test_select(browser):
 
     page.get("http://www.w3school.com.cn/tiy/t.asp?f=html_select")
     page.frame.switch_to_frame()
-    PageSelect(page.elm, value="saab")
+    page.select_elem.select_by_value("saab")
     sleep(2)
-    PageSelect(page.elm, index=2)
+    page.select_elem.select_by_index(2)
     sleep(2)
-    PageSelect(page.elm, text="Audi")
+    page.select_elem.select_by_visible_text("Audi")
     sleep(2)
 
---
+++ sample/selenium_sample/test_window.py
@@ -0,0 +1,35 @@
+from poium import Page, NewPageElement as PageElement
+
+
+class BaiduPage(Page):
+    login_button = PageElement(link_text="登录")
+    register_link = PageElement(link_text="立即注册")
+
+
+def test_switch_windows(browser):
+    page = BaiduPage(browser)
+    page.get("https://www.baidu.com")
+
+    # 当前窗口句柄
+    current_handler = page.current_window_handle
+    print(current_handler)
+
+    # 打开新的注册窗口
+    page.login_button.click()
+    page.register_link.click()
+
+    # 新窗口句柄
+    new_handler = page.new_window_handle
+    print(new_handler)
+
+    # 所有窗口句柄
+    all_handler = page.window_handles
+    print(all_handler)
+
+    # 切换窗口句柄
+    page.switch_to_window(current_handler)
+    page.switch_to_window(new_handler)
+
+    browser.quit()
+
+
