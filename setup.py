c1b3433a8c138040bfb984c161e536f8d749c1b3
defnngj <defnngj@gmail.com>
2020-12-09 23:55:49 +0800

    fix error

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=['selenium>=3.14.1',
                       'Appium-Python-Client>=1.0.1',
                       'colorama>=0.4.3',
-                      'func-timeout=>4.3.5'],
+                      'func-timeout>=4.3.5'],
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: MacOS :: MacOS X',
