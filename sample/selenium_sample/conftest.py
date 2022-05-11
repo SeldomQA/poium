da3a3a7e2bb2dcb45f8e8abfd83bf22ece96f7e4
defnngj <defnngj@gmail.com>
2022-05-12 00:51:42 +0800

    sample: chrome enable logging

--- sample/selenium_sample/conftest.py
+++ sample/selenium_sample/conftest.py
@@ -5,7 +5,10 @@ from selenium import webdriver
 @pytest.fixture(scope='session', autouse=True)
 def browser():
     global driver
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    # to supress the error messages/logs
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    driver = webdriver.Chrome(options=options)
     return driver
 
 
