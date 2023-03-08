0116cac615a4a96f84a3b3a28bd39e444d9a5719
defnngj <defnngj@gmail.com>
2023-03-09 00:57:32 +0800

    feat: update selection_checker

--- poium/selenium.py
+++ poium/selenium.py
@@ -169,23 +169,23 @@ class Element(object):
             # partial_link_text
             k = By.PARTIAL_LINK_TEXT
             v = selector[6:]
-        elif selector.startswith("!") and len(selector) > 1:
+        elif selector.startswith("id=") and len(selector) > 3:
             # id
             k = By.ID
-            v = selector[1:]
-        elif selector.startswith("=") and len(selector) > 1:
+            v = selector[3:]
+        elif selector.startswith("name=") and len(selector) > 5:
             # name
             k = By.NAME
-            v = selector[1:]
-        elif selector.startswith("&") and len(selector) > 1:
+            v = selector[5:]
+        elif selector.startswith("class=") and len(selector) > 6:
             # class name
             k = By.CLASS_NAME
-            v = selector[1:]
-        elif selector.startswith("<") and len(selector) > 1:
+            v = selector[6:]
+        elif selector.startswith("tag=") and len(selector) > 4:
             # tag name
             k = By.TAG_NAME
-            v = selector[1:]
-        elif selector.startswith("/") and len(selector) > 3:
+            v = selector[4:]
+        elif selector.startswith("/") and len(selector) > 1:
             # xpath
             k = By.XPATH
             v = selector
